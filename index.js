80c3a28e5fc5329a44a285d3ed42238d3acc6d28
Abdurrahman Abuzaid <abdurrahman.abzd@gmail.com>
2023-03-21 10:34:15 -0400

    Solve assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(names, eventName){
+    const cards = []; 
+    for(let i=0; i<names.length ; i++){
+        cards.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return cards;
+}
+
+function countDown(number){
+    let ind = number;
+    while(!ind==0){
+        console.log(ind);
+        ind--;
+    }
+    console.log(0);
+}
